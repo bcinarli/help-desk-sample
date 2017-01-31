4dd6276823c31498aebef584f11a806190328067
Bilal Çınarlı <bcinarli@gmail.com>
2017-01-31 23:03:06 +0300

    gh-pages

--- live-chat/source/scripts/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author Bilal Cinarli
- */
-
-import '../styles/styles.scss';
-
-import LiveChat from './modules/livechat';
-
-let liveChat = new LiveChat();
--- source/scripts/views/layout/footer.js
+++
@@ -1,3 +0,0 @@
-/**
- * @author Bilal Cinarli
- */
